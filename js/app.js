0457f29351ab476d65dd1893419684ba24d3bc94
fataranto <fataranto@gmail.com>
2022-05-23 20:47:45 +0200

    versión ok

--- js/app.js
+++ js/app.js
@@ -1,2 +1,28 @@
 // Tu código aquí.
 
+let counter = document.querySelector("#counter");
+console.log(counter);
+let myCounter = parseInt(counter.textContent);
+console.log(myCounter);
+
+let prevBtn = document.querySelector(".prevBtn");
+let nextBtn = document.querySelector(".nextBtn");
+
+
+prevBtn.addEventListener("click", function () {
+    myCounter--;
+    counter.textContent = myCounter;
+
+    setColor();
+})
+
+nextBtn.addEventListener("click", function () {
+    myCounter++;
+    counter.textContent = myCounter;
+
+    setColor();
+})
+
+setColor = function () {
+    myCounter < 0 ? counter.style.color = "red" : myCounter > 0 ? counter.style.color = "green" : counter.style.color = "black";
+}
