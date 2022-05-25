6d58581a7354ae2dd2bbbc8dc16d7f16b337741b
fataranto <fataranto@gmail.com>
2022-05-25 17:31:44 +0200

    versión optimizada

--- js/app.js
+++ js/app.js
@@ -1,28 +1,21 @@
 // Tu código aquí.
 
-let counter = document.querySelector("#counter");
-console.log(counter);
+
 let myCounter = parseInt(counter.textContent);
-console.log(myCounter);
+//let prevBtn = document.querySelector(".prevBtn");
+//let nextBtn = document.querySelector(".nextBtn");
 
-let prevBtn = document.querySelector(".prevBtn");
-let nextBtn = document.querySelector(".nextBtn");
-
-
-prevBtn.addEventListener("click", function () {
-    myCounter--;
+update = function (valor) {
+    let counter = document.querySelector("#counter");
+    myCounter += valor;
     counter.textContent = myCounter;
-
-    setColor();
-})
-
-nextBtn.addEventListener("click", function () {
-    myCounter++;
-    counter.textContent = myCounter;
-
-    setColor();
-})
-
-setColor = function () {
     myCounter < 0 ? counter.style.color = "red" : myCounter > 0 ? counter.style.color = "green" : counter.style.color = "black";
 }
+
+document.querySelector(".prevBtn").addEventListener("click", () => {
+    update(-1);
+});
+
+document.querySelector(".nextBtn").addEventListener("click", () => {
+    update(1);
+});
